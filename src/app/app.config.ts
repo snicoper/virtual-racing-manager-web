c41414533486d25350ea6d32f3c75b372ad216f9
Salvador Nicolas <snicoper@gmail.com>
2026-05-10 22:16:40 +0200

    chore(front): configure Luxon adapter and Angular Material setup

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,19 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import { provideLuxonDateAdapter } from '@angular/material-luxon-adapter';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideLuxonDateAdapter(),
+    {
+      provide: MAT_DATE_LOCALE,
+      useValue: 'es-ES',
+    },
+
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
